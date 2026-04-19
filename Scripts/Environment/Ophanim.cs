38d61caf5cf569a0e9e123c41b6109580fc698d3
BoltyTheDog <boltythedoge@gmail.com>
2026-04-19 11:52:34 +0200

    Ophanim set on Pizzeria

--- Scripts/Environment/Ophanim.cs
+++ Scripts/Environment/Ophanim.cs
@@ -2,6 +2,8 @@ using Godot;
 
 public partial class Ophanim : Node3D
 {
+    [Export] public float AnimationSpeed = 1.75f;
+
     public override void _Ready()
     {
         var animPlayer = FindAnimationPlayer(this);
@@ -9,8 +11,20 @@ public partial class Ophanim : Node3D
         {
             var anim = animPlayer.GetAnimation("Animation");
             anim.LoopMode = Animation.LoopModeEnum.Linear;
+            animPlayer.SpeedScale = AnimationSpeed;
             animPlayer.Play("Animation");
         }
+
+        SetupAudio("DroneAudio");
+        SetupAudio("WingFlapAudio");
+    }
+
+    private void SetupAudio(string nodeName)
+    {
+        var player = GetNodeOrNull<AudioStreamPlayer3D>(nodeName);
+        if (player == null) return;
+        player.Finished += () => player.Play();
+        player.Play();
     }
 
     private AnimationPlayer FindAnimationPlayer(Node node)
